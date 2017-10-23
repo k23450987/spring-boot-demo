7c9aef77ce63df227312604889172b2adfdda234
Tom <xiaoti008@gmail.com>
2017-10-23 08:44:55 +0800

    Update SpringSecUserServiceImpl.java
    add comment

--- src/main/java/com/tom/service/impl/SpringSecUserServiceImpl.java
+++ src/main/java/com/tom/service/impl/SpringSecUserServiceImpl.java
@@ -12,6 +12,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+/*
+    实现 Spring Security 的 UserDetailsService 接口
+*/
+
 public class SpringSecUserServiceImpl implements UserDetailsService {
 
     @Resource
@@ -24,10 +28,16 @@ public class SpringSecUserServiceImpl implements UserDetailsService {
             System.out.println("User not found");
             throw new UsernameNotFoundException("Username not found");
         }
+        // 封装成 Spring Security 的用户类
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
                 true, true, true, true, getGrantedAuthorities(user));
     }
 
+    /**
+    * 获取用户的权限
+    * @param user 用户实体
+    * @return Spring Security 的 GrantedAuthority 权限集合
+    */
     private List<GrantedAuthority> getGrantedAuthorities(User user) {
         List<GrantedAuthority> auths = new ArrayList<>();
         for (Role role : user.getRoles()) {
